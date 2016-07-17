2773f77d7dd45ad5bb78925ecbc530d6aab238bb
Yu Yang <yu.yang2@hpe.com>
2016-07-17 11:32:52 +0800

    part 2 finish

--- hello.py
+++ hello.py
@@ -94,3 +94,34 @@ def addMe2Me(x):
     return (x + x)
 print addMe2Me(4.25)
 
+#类 类的首字母要大写,由于类可以起到模板的作用,因此可以在创建类的实例的时候,把我们属性强制绑定进去
+# 数据封装
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    def print_score(self):
+        print '%s\'s age is: %s' % (self.name, self.score)
+
+bart = Student('Bart Simpson', 59)
+print bart.name
+print bart.score
+bart.print_score()
+
+#模块 
+import sys
+sys.stdout.write('Hello World!\n')
+print sys.platform
+print sys.version
+
+#实用的函数
+#dir([obj]) 显示对象的属性,如果没有提供参数, 则显示全局变量的名字
+#help([obj]) 以一种整齐美观的形式 显示对象的文档字符串, 如果没有提供任何参 数, 则会进入交互式帮助。
+#int(obj) 将一个对象转换为整数
+#len(obj) 返回对象的长度
+#open(fn, mode) 以 mode('r' = 读, 'w'= 写)方式打开一个文件名为 fn 的文件
+#range([[start,]stop[,step]) 返回一个整数列表。起始值为 start, 结束值为 stop - 1; start 默认值为 0, step默认值为1。
+#raw_input(str) 等待用户输入一个字符串, 可以提供一个可选的参数 str 用作提示信 息。
+#str(obj) 将一个对象转换为字符串
+#type(obj) 返回对象的类型(返回值本身是一个 type 对象!)
+
---
+++ practice.py
@@ -0,0 +1,3 @@
+#encoding=UTF-8
+a = 'practice'
+print a
